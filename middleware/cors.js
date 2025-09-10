4179df544acc4822b6ea7b1060f66a4c599e1645
gilad44 <gilstone30@gmail.com>
2025-09-10 03:21:40 +0300

    Update cors.js

--- middleware/cors.js
+++ middleware/cors.js
@@ -4,7 +4,11 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5173"],
+    origin: [
+      "http://127.0.0.1:5500", 
+      "http://localhost:3000",
+      "http://localhost:5173"
+    ],
     optionsSuccessStatus: 200,
   })
 );
